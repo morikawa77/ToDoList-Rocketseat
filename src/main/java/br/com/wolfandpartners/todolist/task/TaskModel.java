60b9d139bfc79d5bdb7170578cc7a487ca92cec4
Reginaldo Morikawa <morikawa77@gmail.com>
2023-10-14 01:00:16 -0300

    Aula 05 - Deploy do Back-End

--- src/main/java/br/com/wolfandpartners/todolist/task/TaskModel.java
+++ src/main/java/br/com/wolfandpartners/todolist/task/TaskModel.java
@@ -30,4 +30,11 @@ public class TaskModel {
   private LocalDateTime createdAt;
 
   private UUID idUser;
+
+  public void setTitle(String title) throws Exception {
+    if (title.length() > 50) {
+      throw new Exception("O título não pode ter mais de 50 caracteres");
+    }
+    this.title = title;
+  }
 }
